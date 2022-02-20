6184e7cb4c1b7fd7ed091ce6e28b7ff268c359ac
Jorgelina Hamui <jorgelinahamui12@gmail.com>
2022-02-20 02:13:42 -0300

    Preparando entrega final, primer commit

---
+++ javascript/todito.js
@@ -0,0 +1,20 @@
+console.log('Para calcular tu presupuesto');
+console.log('Te voy a ayudar a organizarte');
+
+let nombreIngresado = prompt ('Ingresar nombre')
+alert("El nombre ingresado es " + nombreIngresado);
+
+alert(nombreIngresado + ' repasamos las tablas? ');
+
+
+
+let number = prompt ('Dame un numero')
+    for (let i = 1; i<= 10; i++) {
+        const result = number * i
+        // 5 x 1 = 5
+        //5 x 2 = 10
+        console.log(number + ' x ' +  i + "=" + result)
+    
+    
+    }
+    
